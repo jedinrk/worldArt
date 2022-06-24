b8c1718a911174235f29e684b69e1a4f4cad0ed1
jedinrk <jedi.nrk@gmail.com>
2022-06-25 02:13:44 +0400

    structuring react app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { ConnectButton, CryptoCards, Button } from 'web3uikit';
+import { Navbar, Footer } from './components'
 
-function App() {
 
-  return (
-    <div className="App">
-      
+const App = () => (
+  <>
+    <div className='App'>
+      <Navbar />
+      <div className='App-header'>
+        <CryptoCards
+          chain="ethereum"
+          bgColor='blue'
+          chainType='Network'
+          onClick={console.log}
+        />
+        <Button theme="primary" type="button" text="Launch Dapp" />
+
+      </div>
+      <Footer />
     </div>
-  )
-}
+
+  </>
+);
 
 export default App
